e4ad28a736fa75b6743ffb118054dc1703cf6821
Pranta Dutta <prantadutta1997@gmail.com>
2024-06-14 11:21:35 +0600

    changed the dev url

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,5 +1,5 @@
 // const kApiProdUrl = '';
-const kApiDevUrl = 'http://192.168.0.105:5291/api/v1.0';
+const kApiDevUrl = 'http://192.168.0.106:5291/api/v1.0';
 const kApiUrl = kApiDevUrl;
 
 const kGetAllGenres = 'Movie/GetAllGenres';
